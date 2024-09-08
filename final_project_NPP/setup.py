4662d744d76841a6d9b5fb6d8262f60a1f5c6316
Krystian Wrotniak <56126037+wrotniakk@users.noreply.github.com>
2024-09-08 17:19:51 +0200

    Update setup.py

--- final_project_NPP/setup.py
+++ final_project_NPP/setup.py
@@ -1,21 +1,10 @@
 import os
-import zipfile
 from setuptools import setup, find_packages
 
-def unzip_data():
-    """Unzip data.zip to the data directory."""
-    zip_path = os.path.join(os.path.dirname(__file__), 'data', 'data.zip')  # Path to the zip file
-    extract_path = os.path.join(os.path.dirname(__file__), 'data')  # Folder where it will be extracted
-
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(extract_path)
-
-# Call the unzip logic before installing the package
-unzip_data()
 
 setup(
     name='final_project_NPP',
-    version='1.0.0',
+    version='0.1.0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[],
