eba981540b5cb0ac452ddfeb659a19cc750bb583
Krystian Wrotniak <56126037+wrotniakk@users.noreply.github.com>
2024-09-08 16:10:34 +0200

    Update setup.py

--- final_project_NPP/setup.py
+++ final_project_NPP/setup.py
@@ -1,19 +1,26 @@
-# setup.py
-
+import os
+import zipfile
 from setuptools import setup, find_packages
 
+def unzip_data():
+    """Unzip data.zip to the data directory."""
+    zip_path = os.path.join(os.path.dirname(__file__), 'data', 'data.zip')  # Path to the zip file
+    extract_path = os.path.join(os.path.dirname(__file__), 'data')  # Folder where it will be extracted
+
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_path)
+
+# Call the unzip logic before installing the package
+unzip_data()
+
 setup(
-    name='final_project_NPP',              # Name of the package
-    version='1.0.0',                       # Version of the package
-    packages=find_packages('src'),         # Automatically discover packages in the 'src' directory
-    package_dir={'': 'src'},               # Tell distutils packages are in the 'src' directory
-    entry_points={                         
-        'console_scripts': [
-            'analysis=analysis:main',      
-        ],
-    },
-    include_package_data=True,              
-    author='Krystian Wrotniak',             
-    description='Final project assignment for NPP subject',
-    url='https://github.com/wrotniakk/MyProjects/tree/final_project_NPP/final_project_NPP',  # GitHub URL
+    name='final_project_NPP',
+    version='1.0.0',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[],
+    include_package_data=True,
+    author='Krystian Wrotniak',
+    description='The final assignment for NPP subject',
+    url='https://github.com/wrotniakk/MyProjects/tree/final_project_NPP/final_project_NPP',
 )
